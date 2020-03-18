a2683d6aaa8a45583c689f4bcdb4a3979ea3cba4
Georgi Panayotov <g.panayotov@ada-soft.bg>
2020-03-18 18:17:57 +0200

    Bulgarian translation

---
+++ packages/vuetify/src/locale/bg.ts
@@ -0,0 +1,48 @@
+export default {
+  badge: 'Значка',
+  close: 'Затваряне',
+  dataIterator: {
+    noResultsText: 'Няма намерени записи',
+    loadingText: 'Зареждане...',
+  },
+  dataTable: {
+    itemsPerPageText: 'Редове на страница:',
+    ariaLabel: {
+      sortDescending: ': Сортирани низходящо. Активирайте за да сортирате възходящо.',
+      sortAscending: ': Сортирани възходящо. Активирайте за да сортирате низходящо.',
+      sortNone: ': Не сортирани. Активирайте за да сортирате възходящо.',
+    },
+    sortBy: 'Сортиране по',
+  },
+  dataFooter: {
+    itemsPerPageText: 'Записи на страница:',
+    itemsPerPageAll: 'Всички',
+    nextPage: 'Следваща страница',
+    prevPage: 'Предишна страница',
+    firstPage: 'Първа страница',
+    lastPage: 'Последна страница',
+    pageText: '{0}-{1} от {2}',
+  },
+  datePicker: {
+    itemsSelected: '{0} избрани',
+  },
+  noDataText: 'Няма налични данни',
+  carousel: {
+    prev: 'Предишен',
+    next: 'Следващ',
+    ariaLabel: {
+      delimiter: 'Слайд {0} от {1}',
+    },
+  },
+  calendar: {
+    moreEvents: 'още {0}',
+  },
+  fileInput: {
+    counter: '{0} файла',
+    counterSize: '{0} файла ({1} общо)',
+  },
+  timePicker: {
+    am: 'AM',
+    pm: 'PM',
+  },
+}
